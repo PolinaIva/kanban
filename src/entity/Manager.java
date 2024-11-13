70f22b413e1b5948d9c1391b23318ce6e9d15add
U_M19DT <PIvakhnik@alfabank.ru>
2024-11-13 20:58:46 +0300

    correct checkstyle error - 3

--- src/entity/Manager.java
+++ src/entity/Manager.java
@@ -13,9 +13,11 @@ public class Manager {
     public List<Task> getTaskList() {
         return tasks.values().stream().toList();
     }
+
     public List<Epic> getEpicList() {
         return epics.values().stream().toList();
     }
+
     public List<Subtask> getSubtaskList() {
         return subtasks.values().stream().toList();
     }
@@ -23,13 +25,15 @@ public class Manager {
     public void deleteTaskList() {
         tasks.clear();
     }
+
     public void deleteEpicList() {
         epics.clear();
         deleteSubtaskList();
     }
+
     public void deleteSubtaskList() {
         subtasks.clear();
-        if(!epics.isEmpty()) {
+        if (!epics.isEmpty()) {
             for (Epic e : epics.values()) {
                 e.getSubtaskList().clear();
             }
@@ -39,15 +43,16 @@ public class Manager {
     public void deleteEpic(int id) {
         Epic deletedEpic = epics.remove(id);
         if (Objects.nonNull(deletedEpic) && !subtasks.isEmpty()) {
-           List<Integer> subtaskIdList = subtasks.values().stream()
-                   .filter(s -> s.getEpicId() == deletedEpic.getId())
-                   .map(Task::getId)
-                   .toList();
-           for (int i : subtaskIdList) {
-               subtasks.remove(i);
-           }
+            List<Integer> subtaskIdList = subtasks.values().stream()
+                    .filter(s -> s.getEpicId() == deletedEpic.getId())
+                    .map(Task::getId)
+                    .toList();
+            for (int i : subtaskIdList) {
+                subtasks.remove(i);
+            }
         }
     }
+
     public void deleteSubtask(int id) {
         Subtask deletedSubtask = subtasks.remove(id);
         if (Objects.nonNull(deletedSubtask)) {
@@ -55,40 +60,47 @@ public class Manager {
             epics.get(deletedSubtask.getEpicId()).updateStatus();
         }
     }
+
     public void deleteTask(int id) {
-       tasks.remove(id);
+        tasks.remove(id);
     }
 
     public Task getTask(int id) {
         return tasks.get(id);
     }
+
     public Epic getEpic(int id) {
         return epics.get(id);
     }
+
     public Subtask getSubtask(int id) {
         return subtasks.get(id);
     }
 
     public void createTask(Task task) {
-        tasks.put(task.getId(),task);
+        tasks.put(task.getId(), task);
     }
+
     public void createEpic(Epic epic) {
-        epics.put(epic.getId(),epic);
+        epics.put(epic.getId(), epic);
     }
+
     public void createSubtask(Subtask subtask) {
-        subtasks.put(subtask.getId(),subtask);
+        subtasks.put(subtask.getId(), subtask);
         epics.get(subtask.getEpicId()).addSubtask(subtask);
         epics.get(subtask.getEpicId()).updateStatus();
     }
 
     public void updateTask(Task task) {
-        tasks.put(task.getId(),task);
+        tasks.put(task.getId(), task);
     }
+
     public void updateEpic(Epic epic) {
-        epics.put(epic.getId(),epic);
+        epics.put(epic.getId(), epic);
     }
+
     public void updateSubtask(Subtask subtask) {
-        subtasks.put(subtask.getId(),subtask);
+        subtasks.put(subtask.getId(), subtask);
         Epic epic = epics.get(subtask.getEpicId());
         epic.updateStatus();
         updateEpic(epic);
